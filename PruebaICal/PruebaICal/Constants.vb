a6df014dc87f806058b6873f9c100f8af832c208
Juan Manuel Cardeso García <jmcardeso@outlook.es>
2020-04-24 00:21:36 +0200

    Preparando trasvase 2

--- PruebaICal/PruebaICal/Constants.vb
+++
@@ -1,5 +0,0 @@
-﻿Module Constants
-    Public Const MYSQL_DATABASE As String = "world"
-    Public Const MYSQL_CS_NAME As String = "mysql"
-    Public Const MYSQL_PROVIDER_NAME As String = ".NET Framework Data Provider for MySQL"
-End Module
