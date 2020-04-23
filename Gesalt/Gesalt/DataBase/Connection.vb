23a8a5723678072b3fd971b7d139a0971c6a15bc
Juan Manuel Cardeso García <a18juancg@iessanclemente.net>
2020-04-24 00:38:31 +0200

    trasvase de pruebas hecho

---
+++ Gesalt/Gesalt/DataBase/Connection.vb
@@ -0,0 +1,173 @@
+﻿Imports System.Configuration
+Imports MySql.Data.MySqlClient
+Imports System.Data.OleDb
+Imports System.Data.Common
+Public Class Connection
+    Private Shared objConnection As Connection
+    Private Con As DbConnection
+
+    Private Sub New()
+
+    End Sub
+
+    Public Shared Function getInstance() As Connection
+        If objConnection Is Nothing Then
+            objConnection = New Connection()
+        End If
+
+        Return objConnection
+    End Function
+    Public Function Open(cs As MySqlConnectionStringBuilder) As MySqlConnection
+        If Con Is Nothing Then
+            Try
+                Con = New MySqlConnection(cs.ConnectionString)
+                Con.Open()
+            Catch err As Exception
+                Return Nothing
+            End Try
+        End If
+        Return Con
+    End Function
+
+    Public Function Open(cs As OleDbConnectionStringBuilder) As OleDbConnection
+        If Con Is Nothing Then
+            Try
+                Con = New OleDbConnection(cs.ConnectionString)
+                Con.Open()
+            Catch err As Exception
+                Return Nothing
+            End Try
+        End If
+        Return Con
+    End Function
+
+    Public Sub Close()
+        If Not Con Is Nothing Then
+            Con.Close()
+            Con = Nothing
+        End If
+    End Sub
+
+    Public Function DataApdapter(selectCommandText As String, connection As MySqlConnection) As MySqlDataAdapter
+        Return New MySqlDataAdapter(selectCommandText, connection)
+    End Function
+
+    Public Function DataApdapter(selectCommandText As String, connection As OleDbConnection) As OleDbDataAdapter
+        Return New OleDbDataAdapter(selectCommandText, connection)
+    End Function
+    Public Sub EditConnectionString(csName As String, csConnectionString As String, csProviderName As String)
+        If RemoveConnectionStrings(csName) Then
+            AddConnectionString(csName, csConnectionString, csProviderName)
+        End If
+    End Sub
+
+    Private Sub ProtectConnectionString()
+        Dim config As Configuration = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None)
+        Dim cs As ConfigurationSection = config.ConnectionStrings
+
+        If Not cs.SectionInformation.IsProtected Then
+            If Not cs.ElementInformation.IsLocked Then
+                cs.SectionInformation.ProtectSection("DataProtectionConfigurationProvider")
+                cs.SectionInformation.ForceSave = True
+                config.Save(ConfigurationSaveMode.Full)
+                ConfigurationManager.RefreshSection("configuration")
+            End If
+        End If
+    End Sub
+
+    Private Sub UnprotectConnectionString()
+        Dim config As Configuration = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None)
+        Dim cs As ConfigurationSection = config.ConnectionStrings
+
+        If cs.SectionInformation.IsProtected Then
+            If Not cs.ElementInformation.IsLocked Then
+                cs.SectionInformation.UnprotectSection()
+                cs.SectionInformation.ForceSave = True
+                config.Save(ConfigurationSaveMode.Full)
+                ConfigurationManager.RefreshSection("configuration")
+            End If
+        End If
+    End Sub
+
+    ' Ref: https://docs.microsoft.com/en-us/dotnet/api/system.configuration.connectionstringsettingscollection.add?view=netframework-4.8
+    Public Sub AddConnectionString(csName As String, csConnectionString As String, csProviderName As String)
+        Try
+            ' Get the configuration file.
+            Dim config As Configuration = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None)
+
+            ' Add the connection string.
+            Dim csSection As ConnectionStringsSection = config.ConnectionStrings
+            csSection.ConnectionStrings.Add(New ConnectionStringSettings(csName, csConnectionString, csProviderName))
+
+            ' Save the configuration file.
+            config.Save(ConfigurationSaveMode.Modified)
+            ConfigurationManager.RefreshSection("configuration")
+            ProtectConnectionString()
+        Catch err As ConfigurationErrorsException
+            MsgBox(err.BareMessage)
+        End Try
+    End Sub
+
+    ' Ref: https://docs.microsoft.com/en-us/dotnet/api/system.configuration.connectionstringsettingscollection.remove?view=netframework-4.8
+    Private Function RemoveConnectionStrings(csName As String) As Boolean
+        Dim resultado As Boolean = False
+
+        Try
+            UnprotectConnectionString()
+            ' Get the application configuration file.
+            Dim config As Configuration = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None)
+
+            ' Clear the connection strings collection.
+            Dim csSection As ConnectionStringsSection = config.ConnectionStrings
+            Dim csCollection As ConnectionStringSettingsCollection = csSection.ConnectionStrings
+
+            ' Get the connection string setting element
+            ' with the specified name.
+            Dim cs As ConnectionStringSettings = csCollection(csName)
+
+            ' Remove it.
+            If Not (cs Is Nothing) Then
+                ' Remove the element.
+                csCollection.Remove(cs)
+
+                ' Save the configuration file.
+                config.Save(ConfigurationSaveMode.Modified)
+                ConfigurationManager.RefreshSection("configuration")
+                resultado = True
+            End If
+        Catch err As ConfigurationErrorsException
+            MsgBox(err.BareMessage)
+        End Try
+
+        Return resultado
+    End Function
+
+    ' Ref: https://docs.microsoft.com/en-us/dotnet/framework/data/adonet/connection-strings-and-configuration-files
+    ' Retrieves the MySQL connection string.
+    ' Returns Nothing if the name is not found.
+    Public Function GetMySQLConnectionString() As ConnectionStringSettings
+
+        ' Assume failure
+        Dim returnValue As ConnectionStringSettings = Nothing
+
+        Dim config As Configuration = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None)
+
+        ' Clear the connection strings collection.
+        Dim csSection As ConnectionStringsSection = config.ConnectionStrings
+        Dim csCollection As ConnectionStringSettingsCollection = csSection.ConnectionStrings
+
+        ' Get the connection string setting element
+        ' with the specified name.
+        Dim settings As ConnectionStringSettings = csCollection(MYSQL_CS_NAME)
+
+        '' Look for the name in the connectionStrings section.
+        'Dim settings As ConnectionStringSettings = ConfigurationManager.ConnectionStrings(MYSQL_CS_NAME)
+
+        ' If found, return the connection string.
+        If Not settings Is Nothing Then
+            returnValue = settings
+        End If
+
+        Return returnValue
+    End Function
+End Class
---
+++ Gesalt/Gesalt/Form1.vb
@@ -0,0 +1,104 @@
+﻿Imports Ical.Net
+Imports Ical.Net.CalendarComponents
+Imports Ical.Net.DataTypes
+Imports Ical.Net.Serialization
+Imports System.Resources
+Public Class Form1
+
+    Public Sub New()
+        Dim strIdioma As String = My.Settings.language
+        Dim cultura As Globalization.CultureInfo
+
+        ' Si es la primera vez que se inicia la aplicación (y, por tanto, no hay un idioma definido)
+        If strIdioma.Equals("first_start") Then
+            cultura = Threading.Thread.CurrentThread.CurrentUICulture
+
+            Select Case cultura.TwoLetterISOLanguageName
+                Case "es", "gl"
+                    strIdioma = cultura.TwoLetterISOLanguageName
+                Case Else
+                    strIdioma = "en"
+            End Select
+            My.Settings.language = strIdioma
+        End If
+
+        ' Para forzar el cambio de idioma por código
+        Threading.Thread.CurrentThread.CurrentUICulture = Globalization.CultureInfo.GetCultureInfo(strIdioma)
+
+        ' Esta llamada es exigida por el diseñador.
+        InitializeComponent()
+
+        ' Agregue cualquier inicialización después de la llamada a InitializeComponent().
+
+    End Sub
+
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ' Creamos un ResourceManager para el formulario
+        Dim LocRM As New ResourceManager("Gesalt.WinFormStrings", GetType(Form1).Assembly)
+        ' Asignamos a la etiqueta Label2 la cadena con la clave Cosa (como en Android)
+        ' Esto no es necesario hacerlo así, porque ya tenemos el propio formulario con la propiedad Language en varios idiomas
+        ' pero sirve para ver cómo se haría con texto mostrado por código
+        Label2.Text = LocRM.GetString("Cosa")
+
+        Dim now = Date.Now
+        Dim later = now.AddHours(1)
+
+        Dim rrule As New RecurrencePattern(FrequencyType.Daily, 1) With {
+            .Count = 5
+        }
+
+        Dim ce As New CalendarEvent With {
+            .Start = New CalDateTime(now),
+            .End = New CalDateTime(later),
+            .RecurrenceRules = New List(Of RecurrencePattern)
+        }
+        ce.RecurrenceRules.Add(rrule)
+
+        Dim calendar As New Calendar()
+        calendar.Events.Add(ce)
+
+        Dim serializer As New CalendarSerializer()
+        Dim serializerCalendar = serializer.SerializeToString(calendar)
+
+        Label1.Text = serializerCalendar
+
+        ' =========================================================================
+
+        Dim con As Connection = Connection.getInstance()
+
+        Dim dbt As New DuckTypingMySQL()
+        'Dim dbt As New DuckTypingOleDB()
+
+        Dim dtsPruebas As New DataSet
+
+        dbt.dbCon = con.Open(dbt.csBuilder)
+
+        dbt.dtaPrueba = con.DataApdapter("select * from country", dbt.dbCon)
+        dbt.dtaPrueba.Fill(dtsPruebas, "paises")
+
+        DataGridView1.DataSource = dtsPruebas.Tables("paises")
+
+        con.Close()
+    End Sub
+    Public Function PruebaDePruebas(numero As Integer) As Integer
+        Return numero * 2
+    End Function
+
+    Private Sub CambiarIdioma(idioma As String)
+        Threading.Thread.CurrentThread.CurrentUICulture = Globalization.CultureInfo.GetCultureInfo(idioma)
+        Me.Controls.Clear()
+        Me.InitializeComponent()
+        Form1_Load(Nothing, Nothing)
+    End Sub
+
+    Private Sub mnuAyuda_Preferencias_Click(sender As Object, e As EventArgs) Handles mnuAyuda_Preferencias.Click
+        Dim frmPref As New frmPreferencias
+
+        frmPref.ShowDialog()
+
+        If frmPref.CambioIdioma Then
+            CambiarIdioma(My.Settings.language)
+        End If
+    End Sub
+End Class
