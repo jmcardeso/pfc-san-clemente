ce18bf4c73509e44c5a72155697fe273cda1a212
Juan Manuel Cardeso García <a18juancg@iessanclemente.net>
2020-06-10 20:56:08 +0200

    Añadido instalador para nueva versión

--- Gesalt/Gesalt/My Project/AssemblyInfo.vb
+++ Gesalt/Gesalt/My Project/AssemblyInfo.vb
@@ -32,6 +32,6 @@ Imports System.Runtime.InteropServices
 ' mediante el carácter '*', como se muestra a continuación:
 ' <Assembly: AssemblyVersion("1.0.*")>
 
-<Assembly: AssemblyVersion("1.0.0.215")>
-<Assembly: AssemblyFileVersion("1.0.0.215")>
+<Assembly: AssemblyVersion("1.0.1.215")>
+<Assembly: AssemblyFileVersion("1.0.1.215")>
 <Assembly: NeutralResourcesLanguage("en")>
